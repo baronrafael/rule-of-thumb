20dc1ef255a996ec542eac17b8f5d32cd38c1493
baronrafael <baronrafael@hotmail.com>
2022-03-23 12:27:58 -0400

    navbar and banners

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerTopComponent } from './components/banner-top/banner-top.component';
+import { BannerBottomComponent } from './components/banner-bottom/banner-bottom.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 const COMPONENTS = [
+  NavbarComponent,
   HeaderComponent,
+  BannerTopComponent,
+  BannerBottomComponent,
   FooterComponent
 ];
 
