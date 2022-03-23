bf8143f222eb58d3115eaa4353e1d18ca35d5511
baronrafael <baronrafael@hotmail.com>
2022-03-23 17:05:45 -0400

    installing date-fns for formatting some dates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DateFnsModule } from 'ngx-date-fns';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerTopComponent } from './components/banner-top/banner-top.component';
 import { BannerBottomComponent } from './components/banner-bottom/banner-bottom.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+const MODULES = [
+  DateFnsModule
+];
 
 const COMPONENTS = [
   NavbarComponent,
@@ -19,10 +24,12 @@ const COMPONENTS = [
     ...COMPONENTS
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ...MODULES
   ],
   exports: [
-    ...COMPONENTS
+    ...COMPONENTS,
+    ...MODULES
   ]
 })
 export class SharedModule { }
