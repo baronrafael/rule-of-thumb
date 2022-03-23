77372e3273cffc9104878f1cacacb73fca247872
baronrafael <baronrafael@hotmail.com>
2022-03-23 09:00:15 -0400

    importing shared module

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './home/home.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +10,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    SharedModule
   ]
 })
 export class PublicModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 
-
+const COMPONENTS = [
+  FooterComponent
+];
 
 @NgModule({
   declarations: [
-    FooterComponent
+    ...COMPONENTS
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ...COMPONENTS
   ]
 })
 export class SharedModule { }
