67cfb779115dbc67280526b8baddd1112f875813
baronrafael <baronrafael@hotmail.com>
2022-03-25 12:19:21 -0400

    celebrity service logic

--- src/app/core/services/celebrity/celebrity.service.ts
+++ src/app/core/services/celebrity/celebrity.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { Celebrity } from 'src/app/shared/interfaces/celebrity';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CelebrityService {
 
-  constructor() { }
+  celebritysKey: string = 'celebritys';
+
+  private celebritys$ = new BehaviorSubject<Celebrity[] | undefined>(undefined);
+
+  getCelebritys(): Observable<Celebrity[] | undefined> {
+    return this.celebritys$.asObservable();
+  }
+  setCelebritys(celebritys: Celebrity[] | undefined) {
+    this.celebritys$.next(celebritys);
+    localStorage.setItem(this.celebritysKey, JSON.stringify(celebritys))
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOriginalData() {
+    return this.httpClient.get(`${environment.dataURL}`).pipe(map((res: any) => { return res.data; }));;
+  }
+
+  getAllFromStorage() {
+    return localStorage.getItem(this.celebritysKey);
+  }
+
 }
