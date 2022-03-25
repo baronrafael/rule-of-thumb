8ef4e8f423072d5710f6e6190dab49c288747f71
baronrafael <baronrafael@hotmail.com>
2022-03-24 21:28:58 -0400

    cards components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerTopComponent } from './components/banner-top/banner-top.component';
 import { BannerBottomComponent } from './components/banner-bottom/banner-bottom.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CelebrityGridCardComponent } from './components/cards/celebrity-grid-card/celebrity-grid-card.component';
+import { CelebrityListCardComponent } from './components/cards/celebrity-list-card/celebrity-list-card.component';
 
 const MODULES = [
   DateFnsModule
@@ -16,7 +18,9 @@ const COMPONENTS = [
   HeaderComponent,
   BannerTopComponent,
   BannerBottomComponent,
-  FooterComponent
+  FooterComponent,
+  CelebrityGridCardComponent,
+  CelebrityListCardComponent
 ];
 
 @NgModule({
