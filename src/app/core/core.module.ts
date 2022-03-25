d43e9c11cac38800734f4fc6ee041ef6d380ac39
baronrafael <baronrafael@hotmail.com>
2022-03-25 15:59:52 -0400

    adding browser animation module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 const MODULES = [
   CommonModule,
   HttpClientModule,
-
+  BrowserAnimationsModule
 ];
 
 @NgModule({
